4f2c54d9eb82ed0ef1e79c0e9f8c8b5a5a246cf9
leon-k-martin <leon.martin@bih-charite.de>
2024-09-09 17:52:43 +0200

    Refactor test_run_models.py to use ontology API for simulation experiment

---
+++ tvb_ext_ontology/tests/test_run_models.py
@@ -0,0 +1,28 @@
+import unittest
+from tvbo.api import ontology_api
+from tvbo.knowledge.ontology import get_models
+
+
+class TestOntologyAPI(unittest.TestCase):
+
+    def test_simulation_experiment(self):
+        models = get_models().keys()  # Fetch actual models
+        api = ontology_api.OntologyAPI()  # Initialize real API
+
+        for m in models:
+            metadata = {
+                "model": {"label": m, "parameters": {}},
+                "connectivity": {
+                    "parcellation": {"atlas": {"name": "Destrieux"}},
+                    "tractogram": {"label": "PPMI85"},
+                },
+                "coupling": {"label": "Linear"},
+                "integration": {"method": "Heun", "noise": None},
+            }
+            # Configure and run the simulation experiment
+            api.configure_simulation_experiment(metadata)
+            api.experiment.run(simulation_length=10)
+
+
+if __name__ == "__main__":
+    unittest.main()
